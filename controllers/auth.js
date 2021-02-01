86c73e933a4af6014d1feab16c56cb05816630fb
Nikhil Chintawar <chintawarnikhil88@gmail.com>
2021-02-01 08:26:09 +0530

    auth: Handle server errors

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,113 +3,166 @@ const { validationResult } = require("express-validator");
 const jwt = require("jsonwebtoken");
 const expressjwt = require("express-jwt");
 
-const signup = (req, res) => {
-    const errors = validationResult(req)
+const signup = async (req, res) => {
+  try {
+    const errors = validationResult(req);
 
-    if(!errors.isEmpty()){
-        return res.status(422).json({
-            error: errors.array()[0].msg
-        });
+    if (!errors.isEmpty()) {
+      return res.status(422).json({
+        error: errors.array()[0].msg,
+      });
     }
 
-    const user = new User(req.body)
-    user.save((error, user) => {
-        if(error){
-            console.log(error)
-            return res.status(400).json({
-                error: "Sign Up error.Please try again."
-            })
-        }
-        res.json({
-            firstName: user.firstName,
-            lastName: user.lastName,
-            email: user.email,
-            mobileNumber: user.mobileNumber,
-            postalAddress: user.postalAddress,
-            role: user.role,
-            id: user._id
+    const user = new User(req.body);
+    await user.save((error, user) => {
+      if (error) {
+        console.log(error);
+        return res.status(400).json({
+          error: "Sign Up error.Please try again.",
+        });
+      }
+      res.json({
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        mobileNumber: user.mobileNumber,
+        postalAddress: user.postalAddress,
+        role: user.role,
+        id: user._id,
+      });
+    });
+  } catch (error) {
+    console.log("signup", error.message);
+    res
+      .status(500)
+      .json({ error: "There is an error with server. Please try again." });
+  }
+};
 
-        })
-    })
-}
-
-const signin = (req, res) => {
-    const errors = validationResult(req)
+const signin = async (req, res) => {
+  try {
+    const errors = validationResult(req);
     const { email, password } = req.body;
 
-    if(!errors.isEmpty()){
-        return res.status(422).json({
-            error: errors.array()[0].msg
-        });
+    if (!errors.isEmpty()) {
+      return res.status(422).json({
+        error: errors.array()[0].msg,
+      });
     }
 
-
-
-    User.findOne({ email }, (error, user) => {
-        if(error || !user) {
-            return res.status(400).json({
-                error: "Invalid email address.Please enter valid email address."
-            })
-        } else if(!user.authenticate(password)){
-            console.log(user.authenticate(password))
-            return res.status(401).json({
-                error: "Invalid password.Please enter valid password."
-            });
-        } else{
+    await User.findOne({ email }, (error, user) => {
+      if (error || !user) {
+        return res.status(400).json({
+          error: "Invalid email address.Please enter valid email address.",
+        });
+      } else if (!user.authenticate(password)) {
+        console.log(user.authenticate(password));
+        return res.status(401).json({
+          error: "Invalid password.Please enter valid password.",
+        });
+      } else {
         //created token
-        const token = jwt.sign({_id: user._id}, process.env.SECRET)
+        const token = jwt.sign({ _id: user._id }, process.env.SECRET);
         //put token in cookie
-        res.cookie("token", token, {expire: new Date() + 300})
+        res.cookie("token", token, { expire: new Date() + 300 });
         //send response to frontend
-        const { _id, firstName, lastName, email, role, mobileNumber, postalAddress } = user;
-        return res.json({ token, user: { _id, firstName, lastName, email, role, mobileNumber, postalAddress }})
-        }
-    })
-}
-
-const signOut = (req, res) => {
-    res.clearCookie('token')
-    res.json({
-        message: "signed out successfully."
+        const {
+          _id,
+          firstName,
+          lastName,
+          email,
+          role,
+          mobileNumber,
+          postalAddress,
+        } = user;
+        return res.json({
+          token,
+          user: {
+            _id,
+            firstName,
+            lastName,
+            email,
+            role,
+            mobileNumber,
+            postalAddress,
+          },
+        });
+      }
     });
-}
+  } catch (error) {
+    console.log("signin", error.message);
+    res.status(500).json({
+      error: "There is an error with server. Please try again.",
+    });
+  }
+};
+
+const signOut = (_, res) => {
+  try {
+    res.clearCookie("token");
+    res.json({
+      message: "signed out successfully.",
+    });
+  } catch (error) {
+    console.log("signout", error.message);
+    res.status(500).json({
+      error: "There is an error with server. Please try again.",
+    });
+  }
+};
 
 //protected route
 const isSignedIn = expressjwt({
-    secret: process.env.SECRET,
-    userProperty: "auth",
-    algorithms: ['RS256']
+  secret: process.env.SECRET,
+  userProperty: "auth",
+  algorithms: ["sha1", "RS256", "HS256"],
 });
 
 //custom middleware
 const isAuthenticated = (req, res, next) => {
-    let checker = req.profile && req.auth && req.profile._id == req.auth._id
-    // console.log(req.profile._id);
-    // console.log(req.auth._id);
-    // console.log(checker)
-    if(!checker){
-        return res.status(403).json({
-            error: "Access denied."
-        });
-    }
-    next();
-}
+  let checker = req.profile && req.auth && req.profile._id == req.auth._id;
+  // console.log(req.profile._id);
+  // console.log(req.auth._id);
+  // console.log(checker)
+  if (!checker) {
+    return res.status(403).json({
+      error: "Access denied.",
+    });
+  }
+  next();
+
+  //   //   get token from header
+  //   const token = req.header("x-auth-token");
+  //   console.log("token");
+  //   // check if no token is present
+  //   if (!token)
+  //     return res.status(401).json({ error: "Access denied. Please sign in." });
+
+  //   // verify token
+  //   try {
+  //     const decoded = jwt.verify(token, process.env.SECRET);
+  //     req.user = decoded.user;
+  //     next();
+  //   } catch (error) {
+  //     res.status(401).json({ error: "Invalid token" });
+  //   }
+};
 
 const isFarmer = (req, res, next) => {
-//   console.log(req.profile)
-    if(req.profile.role === 0){
-        res.status(403).json({
-            error: "only admin access."
-        })
-    }
-    next();
-}
+  //   console.log(req.profile)
+  if (req.profile.role === 0) {
+    res.status(403).json({
+      error: "only admin access.",
+    });
+  }
+  next();
+};
 
-module.exports =({
-    signup,
-    signin,
-    signOut,
-    isSignedIn,
-    isAuthenticated,
-    isFarmer
-})
+module.exports = {
+  signup,
+  signin,
+  signOut,
+  isSignedIn,
+  isAuthenticated,
+  isFarmer,
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,29 +1,50 @@
 const express = require("express");
 const router = express.Router();
-const {check} = require("express-validator");
-const { signin, signup, signOut } = require("../controllers/auth");
+const { check } = require("express-validator");
+const {
+  signin,
+  signup,
+  signOut,
+  isAuthenticated,
+} = require("../controllers/auth");
 const passport = require("passport");
 
+// @route:  GET api/signup
+// @desc:   sign up user
+// @access: Public
+router.post(
+  "/signup",
+  [
+    check("firstName", "name should be at least 3 characters").isLength({
+      min: 3,
+    }),
+    check("lastName", "name should be at least 3 characters").isLength({
+      min: 3,
+    }),
+    check("email", "email is required").isEmail(),
+  ],
+  signup
+);
 
-router.post("/signup", [
-    check('firstName', 'name should be at least 3 characters').isLength({ min:3 }),
-    check('lastName', 'name should be at least 3 characters').isLength({ min:3 }),
-    check('email', 'email is required').isEmail()
-], signup)
+router.post("/signin", [check("email", "email is required").isEmail()], signin);
 
-router.post("/signin", [
-    check('email', 'email is required').isEmail()
-], signin)
+router.get("/signout", isAuthenticated, signOut);
 
-router.get("/signout", signOut)
+router.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
 
-router.get("/auth/google", passport.authenticate("google", { scope: ["profile", "email"] }));
-
-router.get("/auth/google/shop", 
-    passport.authenticate("google", { failureRedirect: "/signin", session: false }), 
-    (req, res) => {
+router.get(
+  "/auth/google/shop",
+  passport.authenticate("google", {
+    failureRedirect: "/signin",
+    session: false,
+  }),
+  (req, res) => {
     let token = req.user.token;
     res.redirect("http://localhost:3000?token=" + token);
-})
+  }
+);
 
 module.exports = router;
